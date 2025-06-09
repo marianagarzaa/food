57f4249d1ebc3511e86030e10b8c53b83fa6e7f3
marianagarzaa <marianagarza4444@gmail.com>
2025-06-09 19:19:03 -0400

    build out search bar + connect to api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from "react-native";
+import SearchBar from '../components/SearchBar';
+import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
+    const [term, setTerm] = useState('');
+    const [searchApi, results, errorMessage] = useResults();
+    console.log(results);
+
     return <View>
-        <Text>Search Screen</Text>
+        <SearchBar
+            term={term}
+            onTermChange={setTerm}
+            onTermSubmit={() => searchApi(term)}
+        />
+        {errorMessage ? <Text>{errorMessage}</Text> : null}
+        <Text> We have found {results.length} results</Text>
+        <ResultsList title={"Cost Effective"}/>
+        <ResultsList title={"Bit Pricer"}/>
+        <ResultsList title={"Big Spender"}/>
     </View>
 };
 
