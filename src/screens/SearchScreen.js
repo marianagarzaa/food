d9e5c6ed8862ce2887bfbfa6776f5384235e5f20
marianagarzaa <marianagarza4444@gmail.com>
2025-06-10 17:37:49 -0400

    finishing search and page specific nav

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import SearchBar from '../components/SearchBar';
 import useResults from "../hooks/useResults";
 import ResultsList from "../components/ResultsList";
@@ -7,20 +7,37 @@ import ResultsList from "../components/ResultsList";
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
-    console.log(results);
 
-    return <View>
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
+
+    return (
+    <>
         <SearchBar
             term={term}
             onTermChange={setTerm}
             onTermSubmit={() => searchApi(term)}
         />
         {errorMessage ? <Text>{errorMessage}</Text> : null}
-        <Text> We have found {results.length} results</Text>
-        <ResultsList title={"Cost Effective"}/>
-        <ResultsList title={"Bit Pricer"}/>
-        <ResultsList title={"Big Spender"}/>
-    </View>
+        <ScrollView>
+            <ResultsList
+                results={filterResultsByPrice('$')}
+                title={"Cost Effective"}
+            />
+            <ResultsList
+                results={filterResultsByPrice('$$')}
+                title={"Bit Pricer"}
+            />
+            <ResultsList
+                results={filterResultsByPrice('$$$')}
+                title={"Big Spender"}
+            />
+        </ScrollView>
+    </>
+    )
 };
 
 const styles = StyleSheet.create({});
